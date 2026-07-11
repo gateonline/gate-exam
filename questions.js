64ee166ff6d1ad44b81545ad681450b7321b6ae2
aswinkrish8-design <aswin.krish8@gmail.com>
2026-07-11 12:16:31 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,21 +1,17 @@
 const questions = [
-
 {
-question:"What is 2 + 2?",
-options:["1","2","3","4"],
+question:"Which one of the monomers given is used in the synthesis of cellulose? (G-23)",
+options:["Fructose","Lactic acid","Galactose","Glucose"],
 answer:3
 },
-
 {
 question:"Derivative of x^2?",
 options:["x","2x","x^2","1"],
 answer:1
 },
-
 {
 question:"Speed of light?",
 options:["3×10^8","10^8","3×10^6","10^6"],
 answer:0
 }
-
 ]
